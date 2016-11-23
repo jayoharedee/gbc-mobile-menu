b7f775ec93e516ee39ed0607dc587b3a1b03e25c
jordan <jordan.morenstein@georgebrown.ca>
2016-11-23 11:44:30 -0500

    moved modules into src folder

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 import fs from 'fs'
 import fetch from 'fetch'
 
-import SanitizeMenu from './SanitizeMenu'
-import BuildMenu from './BuildMenu'
-import Utils from './Utils'
+import SanitizeMenu from './src/SanitizeMenu'
+import BuildMenu from './src/BuildMenu'
+import Utils from './src/Utils'
 
 const Sanitize = new SanitizeMenu()
 const mobileJson = Sanitize.writeJson()
 
-// const readFileAsync = (filename) => {
-//     return new Promise((resolve, reject) => {
-//         fs.readFile(filename, 'utf-8', (err, data) => {
-//             if (err) {
-//                 reject(err)
-//                 throw err
-//             }
-
-//             resolve(data)
-//         })
-//     })
-// }
-
 Utils.readFileAsync(mobileJson).then((data) => {
     let mobileJson = JSON.parse(data)
-
-
-
-
 
     Utils.loopObj(mobileJson, (key, value) => {
         const items = (value.items)? value.items : null
@@ -36,25 +19,4 @@ Utils.readFileAsync(mobileJson).then((data) => {
         )
 
     })
-
-
-
-
-    // for (let [key, value] of entries(mobileJson)) {
-    //     // console.log(value.ItemId, value.title, value.link, value.pid)
-    //     const items = (value.items)? value.items : null
-
-    //     // console.log(
-    //     BuildMenu.buildLevel1(
-    //         value.ItemId, value.title, value.link, items
-    //     )
-    //     // )
-    // }
 })
-
-// using a generator function
-// function* entries(obj) {
-//     for (let key of Object.keys(obj)) {
-//         yield [key, obj[key]];
-//     }
-// }
